8c51542db8fdf3c52a85ef5a3ebda71231e40b36
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-05-19 19:46:26 +0530

    implements block signing and verification

--- src/block.rs
+++ src/block.rs
@@ -2,8 +2,9 @@ use std::collections::{hash_map::DefaultHasher, BTreeSet};
 use std::hash::{Hash, Hasher};
 use std::iter::FromIterator;
 
+use bincode::serialize;
 use chrono::{DateTime, Utc};
-use libp2p::PeerId;
+use libp2p::identity::ed25519::{Keypair, PublicKey};
 use serde::{Deserialize, Serialize};
 
 use crate::dictionary::DICTIONARY;
@@ -11,7 +12,7 @@ use crate::errors::BlockError;
 
 const BLOCK_WORDSET_LENGTH: usize = 4usize;
 
-#[derive(Debug, Hash, Deserialize, Serialize)]
+#[derive(Clone, Debug, Hash, Deserialize, Serialize)]
 pub struct BlockBody {
     wordset: BTreeSet<String>,
 }
@@ -40,10 +41,10 @@ impl BlockBody {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Block {
     body: BlockBody,
-    proposer: String,
+    proposer: [u8; 32],
     hash: u64,
     parent_hash: u64,
     timestamp: DateTime<Utc>,
@@ -61,14 +62,14 @@ impl Hash for Block {
 impl Block {
     pub fn new(
         wordlist: Vec<String>,
-        proposer: PeerId,
+        proposer: PublicKey,
         parent_hash: u64,
     ) -> Result<Block, BlockError> {
         let body = BlockBody::new(wordlist)?;
 
         let mut block = Block {
             body: body,
-            proposer: proposer.to_base58(),
+            proposer: proposer.encode(),
             parent_hash: parent_hash,
             timestamp: Utc::now(),
             hash: 0,
@@ -79,6 +80,33 @@ impl Block {
         block.hash = hasher.finish();
 
         Ok(block)
+    }
+
+    pub fn sign(self, keypair: &Keypair) -> SignedBlock {
+        let message = serialize(&self).unwrap();
+
+        let signature = keypair.sign(&message);
+
+        SignedBlock {
+            message: self,
+            signature: signature,
+        }
+    }
+}
+
+type Signature = Vec<u8>;
+
+#[derive(Debug)]
+pub struct SignedBlock {
+    message: Block,
+    signature: Signature,
+}
+
+impl SignedBlock {
+    pub fn verify_signature(&self, public_key: &PublicKey) -> bool {
+        let message = serialize(&self.message).unwrap();
+
+        public_key.verify(&message, &self.signature)
     }
 }
 
@@ -94,15 +122,14 @@ mod tests {
             "that".to_string(),
             "this".to_string(),
         ];
-        let proposer = PeerId::random();
+        let proposer = Keypair::generate();
         let mut hasher = DefaultHasher::new();
         hasher.write_u16(1337);
         let parent_hash = hasher.finish();
 
-        let result = Block::new(wordlist, proposer, parent_hash);
-        assert!(result.is_ok());
+        let result = Block::new(wordlist, proposer.public(), parent_hash);
 
-        let _block = result.ok().unwrap();
+        assert!(result.is_ok());
     }
 
     #[test]
@@ -113,12 +140,13 @@ mod tests {
             "that".to_string(),
             "notaword".to_string(),
         ];
-        let proposer = PeerId::random();
+        let proposer = Keypair::generate();
         let mut hasher = DefaultHasher::new();
         hasher.write_u16(1337);
         let parent_hash = hasher.finish();
 
-        let result = Block::new(wordlist, proposer, parent_hash);
+        let result = Block::new(wordlist, proposer.public(), parent_hash);
+
         assert!(result.is_err());
         assert_eq!(result.err().unwrap(), BlockError::InvalidWordset);
     }
@@ -126,13 +154,36 @@ mod tests {
     #[test]
     fn test_invalid_wordset_length() {
         let wordlist = vec!["and".to_string(), "for".to_string(), "that".to_string()];
-        let proposer = PeerId::random();
+        let proposer = Keypair::generate();
         let mut hasher = DefaultHasher::new();
         hasher.write_u16(1337);
         let parent_hash = hasher.finish();
 
-        let result = Block::new(wordlist, proposer, parent_hash);
+        let result = Block::new(wordlist, proposer.public(), parent_hash);
+
         assert!(result.is_err());
         assert_eq!(result.err().unwrap(), BlockError::InvalidWordsetLength);
+    }
+
+    #[test]
+    fn test_sign_verify_block() {
+        let wordlist = vec![
+            "and".to_string(),
+            "for".to_string(),
+            "that".to_string(),
+            "this".to_string(),
+        ];
+        let proposer = Keypair::generate();
+        let mut hasher = DefaultHasher::new();
+        hasher.write_u16(1337);
+        let parent_hash = hasher.finish();
+
+        let result = Block::new(wordlist, proposer.public(), parent_hash);
+        let block = result.ok().unwrap();
+        let signed_block = block.sign(&proposer);
+
+        let not_proposer = Keypair::generate();
+        assert!(signed_block.verify_signature(&proposer.public()));
+        assert_eq!(signed_block.verify_signature(&not_proposer.public()), false);
     }
 }
