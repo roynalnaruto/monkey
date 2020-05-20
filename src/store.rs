790028ae41e4221781c5ac04efc61d14b6165c26
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-05-20 16:50:42 +0530

    Changes to DiskStore and make it concurrent

--- src/store.rs
+++ src/store.rs
@@ -1,32 +1,44 @@
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 use rusty_leveldb::{Options, DB};
 
 use crate::errors::StoreError;
 
 pub struct DiscStore {
-    db: DB,
+    db: Arc<Mutex<DB>>,
 }
 
 impl DiscStore {
     pub fn open(path: &Path) -> Result<Self, StoreError> {
         let db = DB::open(&path, Options::default())?;
 
-        Ok(DiscStore { db: db })
+        Ok(DiscStore {
+            db: Arc::new(Mutex::new(db)),
+        })
     }
 
-    pub fn put(&mut self, k: &[u8], v: &[u8]) -> Result<(), StoreError> {
-        self.db.put(k, v)?;
+    pub fn put(&self, k: &[u8], v: &[u8]) -> Result<(), StoreError> {
+        let store = Arc::clone(&self.db);
+        let mut db = store.lock().unwrap();
+
+        db.put(k, v)?;
 
         Ok(())
     }
 
-    pub fn get(&mut self, k: &[u8]) -> Option<Vec<u8>> {
-        self.db.get(k)
+    pub fn get(&self, k: &[u8]) -> Option<Vec<u8>> {
+        let store = Arc::clone(&self.db);
+        let mut db = store.lock().unwrap();
+
+        db.get(k)
     }
 
-    pub fn flush(&mut self) -> Result<(), StoreError> {
-        self.db.flush()?;
+    pub fn flush(&self) -> Result<(), StoreError> {
+        let store = Arc::clone(&self.db);
+        let mut db = store.lock().unwrap();
+
+        db.flush()?;
 
         Ok(())
     }
@@ -35,35 +47,25 @@ impl DiscStore {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::sync::{Arc, Mutex};
 
     lazy_static! {
-        static ref DISC_STORE: Arc<Mutex<DiscStore>> = {
+        static ref DISC_STORE: DiscStore = {
             let path = Path::new(".data").join(".test").join("testdata");
             let store = DiscStore::open(&path).ok().unwrap();
 
-            Arc::new(Mutex::new(store))
+            store
         };
     }
 
     #[test]
     fn test_put() {
-        let store = Arc::clone(&DISC_STORE);
-        let mut store = store.lock().unwrap();
-
-        assert!(store.put(&[1, 2], &[2, 3]).is_ok());
+        assert!(DISC_STORE.put(&[1, 2], &[2, 3]).is_ok());
     }
 
     #[test]
     fn test_get() {
-        let store = Arc::clone(&DISC_STORE);
-        let mut store = store.lock().unwrap();
-
-        assert!(store.put(&[100, 101], &[11, 13]).is_ok());
-
-        let v = store.get(&[100, 101]);
-        assert_eq!(v, Some(vec![11, 13]));
-
-        assert!(store.get(&[10, 11]).is_none());
+        assert!(DISC_STORE.put(&[100, 101], &[11, 13]).is_ok());
+        assert_eq!(DISC_STORE.get(&[100, 101]), Some(vec![11, 13]));
+        assert!(DISC_STORE.get(&[10, 11]).is_none());
     }
 }
